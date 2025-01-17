b7dc1748198fb9d6e093abff596de4f92260fcf9
ideaPad Gaming 3 <boryana.bocheva2003@abv.bg>
2025-01-17 13:38:11 +0100

    Exit class modified, not visible yet, because exits should not be visible

--- core/src/de/tum/cit/ase/bomberquest/map/Exit.java
+++ core/src/de/tum/cit/ase/bomberquest/map/Exit.java
@@ -1,5 +1,6 @@
 package de.tum.cit.ase.bomberquest.map;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -11,41 +12,98 @@ import com.badlogic.gdx.physics.box2d.World;
 /**
  * Represents the exit point in the game.
  */
-public class Exit {
-    /*
-        private final Body body;
-        private final TextureRegion textureRegion;
-        private static final float EXIT_SIZE = 1.0f;
+public class Exit extends Object{
+    /** A flag indicating whether the exit has been unlocked (all enemies defeated). */
+    private boolean unlocked;
 
-        public Exit(World world, float x, float y, TextureRegion textureRegion) {
-            this.textureRegion = textureRegion;
+    /** A flag indicating whether the exit is revealed after the wall is destroyed. */
+    private boolean revealed;
 
+    /** The wall above the exit (a destructible wall). */
+    private WallPath wallAboveExit;
 
-            BodyDef bodyDef = new BodyDef();
-            bodyDef.type = BodyDef.BodyType.StaticBody;
-            bodyDef.position.set(x, y);
-
-            body = world.createBody(bodyDef);
-
-
-            PolygonShape shape = new PolygonShape();
-            shape.setAsBox(EXIT_SIZE / 2, EXIT_SIZE / 2);
-            body.createFixture(shape, 0.0f);
-            shape.dispose();
-        }
-
-
+    /**
+     * Constructs an Exit object with the specified position, dimensions, animation, and wall above it.
+     *
+     * @param x           The x-coordinate of the exit.
+     * @param y           The y-coordinate of the exit.
+     * @param animation   The animation representing the exit's appearance.
+     * @param wallAboveExit  The wall above the exit that must be destroyed to reveal the exit.
      */
-        /**
-         * Renders the exit on the screen.
-         *
-         * @param batch The SpriteBatch used for rendering.
-         */
-        /*
-    public void render(SpriteBatch batch) {
-        Vector2 position = body.getPosition();
-        batch.draw(textureRegion, position.x - EXIT_SIZE / 2, position.y - EXIT_SIZE / 2, EXIT_SIZE, EXIT_SIZE);
+    public Exit(float x, float y, Animation<TextureRegion> animation, WallPath wallAboveExit) {
+        super(x, y, animation);
+        this.wallAboveExit = wallAboveExit;
+        this.unlocked = false;
+        this.revealed = false;
     }
 
-         */
+    /**
+     * Unlocks the exit after all enemies are defeated in the maze.
+     */
+    public void unlockExit() {
+        this.unlocked = true;
+    }
+
+    /**
+     * Reveals the exit by destroying the wall above it.
+     */
+    public void revealExit() {
+        if (wallAboveExit != null && wallAboveExit.isDestroyed()) {
+            this.revealed = true;
+        }
+    }
+
+    /**
+     * Checks if the exit is accessible (unlocked and revealed).
+     *
+     * @return True if the exit is unlocked and revealed, false otherwise.
+     */
+    public boolean isAccessible() {
+        return unlocked && revealed;
+    }
+
+    /**
+     * Updates the exit based on the elapsed time since the last update.
+     * This method also checks if the wall above it is destroyed and updates the revealed status.
+     *
+     * @param delta  The time elapsed since the last update.
+     */
+    @Override
+    public void update(float delta) {
+        super.update(delta);
+        // Check if the exit should be revealed after the wall is destroyed.
+        revealExit();
+    }
+
+    /**
+     * Renders the exit using the specified SpriteBatch.
+     * If the exit is revealed, the exit will be drawn.
+     * If not, the exit will remain hidden beneath the destructible wall.
+     *
+     * @param spriteBatch  The SpriteBatch used for rendering.
+     */
+    @Override
+    public void draw(SpriteBatch spriteBatch, float x, float y, float delta, float width, float height) {
+        if (revealed && isAccessible()) {
+            // Draw the exit only if it's revealed and accessible
+            super.draw(spriteBatch, x, y, delta, width, height);
+        }
+    }
+
+    // Getters and setters
+    public boolean isUnlocked() {
+        return unlocked;
+    }
+
+    public boolean isRevealed() {
+        return revealed;
+    }
+
+    public WallPath getWallAboveExit() {
+        return wallAboveExit;
+    }
+
+    public void setWallAboveExit(WallPath wallAboveExit) {
+        this.wallAboveExit = wallAboveExit;
+    }
 }
